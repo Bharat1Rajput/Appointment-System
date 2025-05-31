7bf286f690c01cb62a63ee04434c9f7f22788621
Bharat Singh <br29112003@gmail.com>
2025-05-31 19:46:06 +0530

    add user auth with signup and login apis

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-
+const authRoutes = require('./routes/authRoutes');
 const app = express();
+
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to the backend server!');
 });
+
 
 // Connect to MongoDB
 mongoose
