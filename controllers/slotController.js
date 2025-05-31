9bf4076316c0cb059f05445a4db18ae6716e75d4
Bharat Singh <br29112003@gmail.com>
2025-05-31 23:38:18 +0530

    create the slot apis and RBAC

---
+++ controllers/slotController.js
@@ -0,0 +1,68 @@
+const Slot = require('../models/slot');
+
+exports.createSlot = async (req,res)=>{
+    const {date,startTime,endTime} = req.body;
+    console.log('Creating slot with data:', { date, startTime, endTime });
+    try {
+        const newSlot = new Slot({
+            date,
+            startTime,
+            endTime,
+            providerId: req.user.userId
+        
+        });
+        console.log('Creating new slot:', newSlot);
+        await newSlot.save();
+        res.status(201).json({ message: 'Slot created successfully', slot: newSlot });
+    } catch (error) {
+        console.error('Error creating slot:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+exports.getSlots = async (req, res) => {
+    try {
+        const slots = await Slot.find({ providerId: req.user.userId }).populate('providerId', 'name email');
+        res.status(200).json(slots);
+    } catch (error) {
+        console.error('Error fetching slots:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+exports.updateSlot = async (req, res) => {
+    const { slotId } = req.params;
+    const { date, startTime, endTime } = req.body;
+
+    try {
+        const updatedSlot = await Slot.findByIdAndUpdate(
+            slotId,
+            { date, startTime, endTime }
+        );
+
+        if (!updatedSlot) {
+            return res.status(404).json({ message: 'Slot not found' });
+        }
+
+        res.status(200).json({ message: 'Slot updated successfully', slot: updatedSlot });
+    } catch (error) {
+        console.error('Error updating slot:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+exports.deleteSlot = async (req, res) => {
+    const { slotId } = req.params;
+    try {
+        const deletedSlot = await Slot.findByIdAndDelete(slotId);
+
+        if (!deletedSlot) {
+            return res.status(404).json({ message: 'Slot not found' });
+        }
+
+        res.status(200).json({ message: 'Slot deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting slot:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
